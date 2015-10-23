5352d239c15124a29de19aeb815fbf468a40613b
Richo <richo2192000@gmail.com>
2015-10-23 16:21:48 +0800

    Create helpers needed for the web service.

--- app.rb
+++ app.rb
@@ -1,8 +1,24 @@
 require 'sinatra/base'
+require_relative './model/userdata'
+require_relative './model/bookinfo'
 
 # Simle web service for taaze api
 class BuezeApp < Sinatra::Base
   helpers do
+    def get_collections(user_id)
+    rescue
+      halt 404
+    end
+
+    def get_comments(user_id)
+    rescue
+      halt 404
+    end
+
+    def get_tags(product_id)
+    rescue
+      halt 404
+    end
   end
 
   get '/' do
