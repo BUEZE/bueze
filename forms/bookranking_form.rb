771631f5f3c4ce1c8d4daa31a2bff4b878a17c9e
Hiraku <dev@hiraku.tw>
2015-11-22 21:23:18 +0800

    refactoring

--- forms/bookranking_form.rb
+++ forms/bookranking_form.rb
@@ -25,22 +25,22 @@ class BookrankingForm
   include ActiveModel::Validations
 
   attribute :booknames, StringStripped
-  attribute :rank, StringStripped
-  attribute :price_description, StringStripped
-  attribute :price, StringStripped
-  attribute :author, StringStripped
+  # attribute :rank, StringStripped
+  # attribute :price_description, StringStripped
+  # attribute :price, StringStripped
+  # attribute :author, StringStripped
   attribute :date, Date
-  attribute :source, StringStripped
-  attribute :prod_id, StringStripped
+  # attribute :source, StringStripped
+  # attribute :prod_id, StringStripped
 
   validates :booknames, presence: true
-  validates :rank, presence: true
-  validates :price_description, presence: true
-  validates :price, presence: true
-  validates :author, presence: true
+  # validates :rank, presence: true
+  # validates :price_description, presence: true
+  # validates :price, presence: true
+  # validates :author, presence: true
   validates :date, presence: true
-  validates :source, presence: true
-  validates :prod_id, presence: true
+  # validates :source, presence: true
+  # validates :prod_id, presence: true
 
   def error_fields
     errors.messages.keys.map(&:to_s).join(', ')
